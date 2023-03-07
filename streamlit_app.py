d3ad0072db1ea6cfff928ca071b5b5a717554128
azielinskiACC <30433567+azielinskiACC@users.noreply.github.com>
2023-03-07 17:05:24 +0100

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 16:27:27 2023
+
+https://blog.streamlit.io/host-your-streamlit-app-for-free/
+
+1. https://streamlit.io/cloud
+"""
+
+import streamlit as st
+
+st.write('Hello world!')
