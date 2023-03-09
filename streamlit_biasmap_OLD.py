8282e5a6811d96fcb00acc44e330b4d70e65272d
azielinskiACC <30433567+azielinskiACC@users.noreply.github.com>
2023-03-09 21:37:17 +0100

    Add files via upload

---
+++ streamlit_biasmap_OLD.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 19:06:12 2023
+
+@author: https://github.com/arnaudmiribel/bias-map/blob/main/streamlit_app.py
+"""
+
+import json
+#import re
+#import urllib
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import random
+import string
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+
+
+st.set_page_config(layout="centered", page_title="Sentiment")
+
+@st.experimental_singleton
+def get_classifier():
+    return pipeline("sentiment-analysis", model='oliverguhr/german-sentiment-bert', tokenizer='oliverguhr/german-sentiment-bert', top_k=3)
+
+@st.experimental_memo(show_spinner=False)
+def predict(reviews):
+    return classifier(reviews)
+
+def result_to_positive_class_probability(result):
+        return result["score"] if result["label"] == "POSITIVE" else 1 - result["score"]
+
+classifier = get_classifier()
+
+st.write("""Discover the Sentiment""")
+
+# =============================================================================
+# text_input = st.text_input(
+#     label="Type in a German news article. "
+# )
+# =============================================================================
+text_input = st.text_area("Type in a German news article",max_chars = 500)
+st.write('About you:', text_input)
+
+
+
+st.write("---")
+
+if len(text_input) > 1 :
+    st.caption("Output:")
+
+
+    with st.spinner("Computing probabilities..."):
+        reviews = []
+        reviews.append(text_input)
+
+        results = predict(reviews)
+        probas_pos = map(result_to_positive_class_probability, results)
+
+        probas_df = pd.DataFrame(
+            {"Positive class probability": probas_pos}
+        )
+
+        st.write("Data (sorted by ascending 'positive' probability):")
+        st.dataframe(probas_df.sort_values(by="Positive class probability", ascending=True), height=350,)
